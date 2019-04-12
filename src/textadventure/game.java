452f183442a75e6777dfe49289879dc15a6ef4c7
rober <rober@DESKTOP-47SCE1I.fritz.box>
2019-04-13 00:10:08 +0200

    hallo

---
+++ src/textadventure/game.java
@@ -0,0 +1,31 @@
+package textadventure;
+import java.util.Scanner;
+
+
+public class game {
+
+	public static void main(String[] args) {
+		
+		var sc = new Scanner(System.in);  
+		
+		
+		System.out.println("Möchtest du Das Spiel Starten Ja/Nein");
+		
+	    if (sc.nextLine() .equalsIgnoreCase("Ja")) {
+	    	System.out.println("SpielStart"); 
+	    
+	    }else {
+		}if (sc.nextLine() .equalsIgnoreCase("Nein"))
+			System.out.println("Dann Schließt sich das spiel"); 
+			System.exit(0);
+	
+		
+	    	
+	/*
+		System.out.print("Setze dein Characternamen :"); 
+		String name = sc.nextLine().replaceAll(" ","");
+		System.out.println(name);
+*/	}
+	}
+
+
