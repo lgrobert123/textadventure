cd3c93e7760ddbbd0640cb6c02cc49261f5e7e80
rober <rober@DESKTOP-47SCE1I.fritz.box>
2019-04-13 01:36:12 +0200

    asdasdasasd

--- src/textadventure/game.java
+++ src/textadventure/game.java
@@ -1,43 +1,38 @@
 package textadventure;
-import java.util.Scanner;
 
+import java.util.Scanner;
 
 public class game {
 
 	public static void main(String[] args) {
-		
-		var sc = new Scanner(System.in);  
-		
+
+		var sc = new Scanner(System.in);
+
 		boolean widerholung = false;
-		
-		
-		
+
 		System.out.println("Möchtest du Das Spiel Starten Ja/Nein");
 
-		while(widerholung == false){
-			
-		String answer = sc.nextLine();
-		
-		
-			if	(answer .equalsIgnoreCase("Ja")) {
-				System.out.println("SpielStart"); 
+		while (widerholung == false) {
+
+			String answer = sc.nextLine();
+
+			if (answer.equalsIgnoreCase("Ja")) {
+				System.out.println("SpielStart");
 				widerholung = true;
-	    
-	  
-			}if	(answer .equalsIgnoreCase("Nein")) {
-				System.out.println("Dann Schließt sich das spiel"); 
+
+			}
+			if (answer.equalsIgnoreCase("Nein")) {
+				System.out.println("Dann Schließt sich das spiel");
 				System.exit(0);
-			 
-			}if	(!answer .equalsIgnoreCase("Ja")){
+
+			}
+			if (!answer.equalsIgnoreCase("Ja")) {
 				System.out.println("Die Antwort wir Verweigert");
 				System.out.println("Möchtest du Das Spiel Starten Ja/Nein");
-					}
-				}
 			}
+		}
+	}
 	/*
-		System.out.print("Setze dein Characternamen :"); 
-		String name = sc.nextLine().replaceAll(" ","");
-		System.out.println(name);
-*/		}
-	
-
+	 * System.out.print("Setze dein Characternamen :"); String name =
+	 * sc.nextLine().replaceAll(" ",""); System.out.println(name);
+	 */ }
