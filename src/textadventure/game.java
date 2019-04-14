fcebf76ac688f0dc3c1d3c5664674357f47ffd07
rober <rober@192.168.0.241>
2019-04-14 16:53:15 +0200

    1.00 Charname Kann gesetzt werden, Man kann ein Charcternamen Machen Ohne Eingabe

--- src/textadventure/game.java
+++ src/textadventure/game.java
@@ -9,6 +9,9 @@ public class game {
 		var sc = new Scanner(System.in);
 
 		boolean widerholung = false;
+		boolean charnameset = false;
+
+		String Helper = "Pipi: ";
 
 		System.out.println("Möchtest du Das Spiel Starten Ja/Nein");
 
@@ -17,22 +20,43 @@ public class game {
 			String answer = sc.nextLine();
 
 			if (answer.equalsIgnoreCase("Ja")) {
-				System.out.println("SpielStart");
+				System.out.println("SpielStart"); // Spiel wird Gestartet
 				widerholung = true;
 
 			}
 			if (answer.equalsIgnoreCase("Nein")) {
 				System.out.println("Dann Schließt sich das spiel");
-				System.exit(0);
+				System.exit(0); // Spiel wird Bendet
 
 			}
-			if (!answer.equalsIgnoreCase("Ja")) {
+			if (!answer.equalsIgnoreCase("Ja")) { // Kein richtige antwort = while
 				System.out.println("Die Antwort wir Verweigert");
 				System.out.println("Möchtest du Das Spiel Starten Ja/Nein");
+
 			}
 		}
+
+		while (charnameset == false) { // Charactername wir gesetzt
+
+			System.out.println("Sätze deinen Characternamen");
+			String Charname = sc.nextLine().replaceAll(" ", "");
+			System.out.println("Soll das dein Charactername sein " + "'" + Charname + "'");
+			System.out.println("Ja/Nein");
+			String answersetCharname = sc.nextLine();
+
+			if (answersetCharname.equalsIgnoreCase("Ja")) {
+				System.out.println("Bestätigt");
+				charnameset = true;
+
+			}
+		}
+
+		System.out.println(Helper +"Hallo" + Charname + "Ich Bin ein Geist Ein kleiner Begleiter Der Dir Hilft");
+
 	}
-	/*
-	 * System.out.print("Setze dein Characternamen :"); String name =
-	 * sc.nextLine().replaceAll(" ",""); System.out.println(name);
-	 */ }
+}
+
+/*
+ * System.out.print("Setze dein Characternamen :"); String name =
+ * sc.nextLine().replaceAll(" ",""); System.out.println(name);
+ */
